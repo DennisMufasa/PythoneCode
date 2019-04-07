1ac52c6a90db85d1c5db8c976fec6cc2abbb279d
DennisMufasa <denny.muasa@gmail.com>
2019-04-07 19:15:50 +0300

    add comment to make code easier to understand

--- primeNo.py
+++ primeNo.py
@@ -1,4 +1,5 @@
-
+"""Printing prime numbers,
+all() returns true if all elements in a loop are true"""
 for i in range(20,50):
     if all(i%p for p in range(2,9)):
         print(i)
--- primefunction.py
+++ primefunction.py
@@ -1,5 +1,6 @@
 def prime(x,y):
     for i in range(x,y):
+        """all() returns true if all elements in a loop are true"""
         if all(i%p for p in range(2,9)):
             prime=i
             print(prime)
